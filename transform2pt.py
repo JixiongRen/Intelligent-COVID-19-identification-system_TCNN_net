d5573333c8c8930cec2aaa3a38cc0a7a4950ab91
renjixiong <renjixiong@126.com>
2023-05-25 22:51:00 +0800

    日常训练日志

--- transform2pt.py
+++ transform2pt.py
@@ -1,4 +1,5 @@
 import torch.utils.data.distributed 
+import numpy as np
 
 '定义转化后的模型名称'
 model_TCNN_pt = 'tcnn4_for_android.pt'
@@ -17,3 +18,12 @@ input_tensor = torch.randn(1, 1, 1, 32000)
 '转化模型并存储'
 model_TCNN_pth = torch.jit.trace(model_TCNN_pth, input_tensor) # type: ignore
 model_TCNN_pth.save(model_TCNN_pt) # type: ignore
+
+
+# from ModuleFunctions import toolsFun
+# cnf_matrix = np.array([[3021, 1446], [1531, 2962]])
+# toolsFun.plot_confusion_matrix(cnf_matrix,
+#                                classes=['negative', 'positive'],
+#                                normalize=False,
+#                                title='Normalized confusion matrix',
+#                                path='new_cm.svg')
